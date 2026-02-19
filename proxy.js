fabf896ce6c7d5ebaa6550dc7217d6ae65ef8e1d
terrell0780 <terrell0780@gmail.com>
2026-02-19 00:57:45 -0800

    save

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse } from 'next/server';
 
-export async function middleware(request) {
+export async function proxy(request) {
     let response = NextResponse.next({
         request: {
             headers: request.headers,
